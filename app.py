00b1cae8e71d81951fbcc95fb3c0ca1e8fbe55c8
vagnercazarotto <vagnercazarotto@gmail.com>
2018-03-10 10:42:24 +0100

    Calling the API from JavaScript

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request , render_template
 
 
 app = Flask(__name__)
@@ -17,6 +17,12 @@ stores = [
 
 # POST -> used to receive data
 # GET -> used to send data back only
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+
 
 # POST /store data: {name:}
 @app.route('/store',methods=['POST'])

4339e6bb28a3b251f9d1fcfe22b1d51c72939af1
vagnercazarotto <vagnercazarotto@gmail.com>
2018-03-22 21:08:15 +0100

    Retrieving many items from the database

--- code/item.py
+++ code/item.py
@@ -94,4 +94,18 @@ class Item(Resource):
 
 class ItemList(Resource):
 	def get(self):
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		query = "SELECT * FROM items"
+		result = cursor.execute(query)
+
+		items = []
+
+		for row in result:
+			items.append({'name': row[0] , 'price': row[1]})
+
+		connection.commit()
+		connection.close()
+
 		return {'item':items}

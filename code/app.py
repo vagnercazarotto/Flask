84e03a50b01454f16c178b82fa5e2bd44e4f7021
vagnercazarotto <vagnercazarotto@gmail.com>
2018-03-27 12:07:55 +0200

    No more creating tables manually—telling SQLAlc

--- code/app.py
+++ code/app.py
@@ -7,10 +7,14 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite://data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+	db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 

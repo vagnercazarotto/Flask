bba26b441417b1bb83d1c7ba4a12514b2c56675c
vagnercazarotto <vagnercazarotto@gmail.com>
2018-06-15 08:22:17 +0200

    Getting the JWT identity

--- code/resources/item.py
+++ code/resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 class Item(Resource):
@@ -69,8 +69,15 @@ class Item(Resource):
 
 
 class ItemList(Resource):
-    TABLE_NAME = 'items'
-
+    
+    @jwt_optional
     def get(self):
 		#return {'item': [item.json() for item in ItemModel.query.all()]}
-        return {'item': [x.json() for x in ItemModel.find_all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'item': [item['name'] for item in items],
+            'message':'More data available if you log in.'
+        }, 200

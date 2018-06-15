955b3f0c48e6340f1a4e9570096d9c8ba4660173
vagnercazarotto <vagnercazarotto@gmail.com>
2018-06-15 10:24:54 +0200

    Blacklisting with Flask-JWT-Extended

--- code/app.py
+++ code/app.py
@@ -1,15 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] =  True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = 'jose'
 api = Api(app)
 
@@ -18,6 +21,10 @@ def create_tables():
 	db.create_all()
 
 jwt = JWTManager(app)  
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+	return decrypted_token['identity'] in BLACKLIST
 
 @jwt.user_claims_loader
 def add_claims_to_jwt(identity):
